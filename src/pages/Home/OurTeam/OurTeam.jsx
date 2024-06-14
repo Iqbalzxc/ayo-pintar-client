6fff54cda8840b6efd6651597bc5ce7185f88744
MuhammadLeon <muhammad25leon@gmail.com>
2024-06-14 16:01:43 +0700

    AboutUs

---
+++ src/pages/Home/OurTeam/OurTeam.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const OurTeam = () => {
+  return (
+    <div className="md:w-[80%] mx-auto my-32">
+      <h1 className="text-4xl font-bold text-center dark:text-white">
+        <span className="text-secondary">Team</span> Kami
+      </h1>
+    </div>
+  );
+};
+
+export default OurTeam;
